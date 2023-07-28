09343f9724f944549864bae8f9b85cf7ec20febe
Nouhailaelmir <nouhailaelmir4@gmail.com>
2023-07-28 15:53:45 -0700

    commit

--- main.c
+++ main.c
@@ -1,26 +1,24 @@
 #include "shell.h"
-int main(int argc, char *argv[])
+/**
+* main - Entry point for the simple shell program.
+*
+* Return: Always 0.
+*/
+int main(void)
 {
 info_t info = INFO_INIT;
-(void)argc;
-(void)argv;
-/* Initialiser les structures de données ou */
-/* les variables nécessaires ici */
+signal(SIGINT, sigintHandler);
+populate_env_list(&info);
 while (1)
 {
-/* Afficher l'invite de commandes du shell ou tout autre message ici */
-/* Lire la ligne d'entrée de l'utilisateur */
+_puts("$ ");
 if (get_input(&info) == -1)
 break;
-/* Traiter la ligne d'entrée */
 find_structure(&info);
-find_cmd(&info);
-/* Gérer éventuellement les enchaînements de commandes ici */
-/* Exécuter les commandes */
-fork_cmd(&info);
-/* Libérer toute mémoire allouée dynamiquement */
-/* ou effectuer le nettoyage ici */
+clear_info(&info);
 }
-/* Effectuer éventuellement le nettoyage final ici */
+write_history(&info);
+free_info(&info, 1);
 return (0);
 }
+
